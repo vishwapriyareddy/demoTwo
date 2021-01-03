ce61159b07103100a68f346c403fd8e8ebf912b1
vishwapriyareddy <vishwapriyareddy24798@gmail.com>
2021-01-03 13:24:04 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
+export default function App() {
+
+const [courseGoals, setCourseGoals] = useState([]);
+
+  
+
+   const addGoalHandler = goalTitle => {
+     setCourseGoals(currentGoals =>[...currentGoals,{id:Math.random().toString(),value:goalTitle}]);
+   };
+
+
+const removeGoalHandler = goalID => {
+  setCourseGoals(currentGoals =>{
+ return currentGoals.filter((goal) => goal.id !== goalID);
+  });
+}
+
+
+
+
+return (
+   
+    <View style={styles.screen}>
+<GoalInput onAddGoal = {addGoalHandler}/> 
+ <FlatList 
+ keyExtractor={(item,index) => item.id}
+ data = {courseGoals} renderItem={itemData =>
+ <GoalItem  id={itemData.item.id} onDelete={removeGoalHandler} title ={itemData.item.value} />}
+   
+
+   />
+   </View>
+    
+  );
+}
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }
+  
+ 
+});
+
